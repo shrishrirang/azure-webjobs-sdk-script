21e341d3685f9a4b484b241a8f8f290b6155bad7
Shrirang Shirodkar <shrirs@microsoft.com>
2017-11-15 09:16:12 -0800

    Hack for nodejs

--- src/WebJobs.Script/azurefunctions/functions.js
+++ src/WebJobs.Script/azurefunctions/functions.js
@@ -1,6 +1,32 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+// *HACK BEGIN*
+// Temporary workaround for https://github.com/nodejs/node/issues/8897
+//
+// In standalone mode, c:\home is a junction which maps to a directory on the host (docker run -v c:\home:c:\home <container name>)
+// This hack filters all requests to fs.realpathSync and special cases the result for c:\home 
+
+var scriptMode = process.env['AzureWebJobsScriptMode'];
+
+if (scriptMode !== undefined && scriptMode.toLowerCase() === 'standalone') {
+    var fs = require('fs');
+
+    var originalFunc = fs.realpathSync;
+
+    fs.realpathSync = function () {
+        var path = arguments[0];
+        // For now, just 
+        if (path !== undefined && (path.toLowerCase().indexOf('c:\\home') === 0 || path.toLowerCase().indexOf('c:\\home\\') === 0)) {
+            return path;
+        }
+
+        return originalFunc.apply(this, arguments);
+    }
+}
+
+// *HACK END*
+
 var util = require('util');
 var process = require('process');
 var request = require('./http/request');
